65a1964e8f3ee2f83d96278e321bb9f9817fab4d
Mituha Mizuki <mituha@restar.jp>
2025-07-07 20:45:17 +0900

    設定初期値変更

--- shinLapediaSettings.ts
+++ shinLapediaSettings.ts
@@ -12,12 +12,12 @@ export interface ShinLapediaPluginSettings {
 }
 
 export const DEFAULT_SETTINGS: ShinLapediaPluginSettings = {
-	geminApiKey: 'default',
+	geminApiKey: '',
 
 	bookFolder: '',
-	bookTitle: '',
-	bookDescription: '',
+	bookTitle: 'ダンジョン辞典',
+	bookDescription: '現代に突如現れたダンジョンに関する情報をまとめた攻略本とも言える辞典。\nダンジョンの構造やモンスター、アイテムなどの情報が網羅されている。\nこの辞典は、ダンジョン探索者や研究者にとって貴重な資料となる。\nダンジョンの発生によりゲームのRPGのような剣と魔法のファンタジーな世界が現実となった。\nこの辞典は、ダンジョンの謎を解明し、探索者たちの安全を確保するための重要なツールである。\n\nこの辞典は、ダンジョンの発生によりゲームのRPGのような剣と魔法のファンタジーな世界が現実となった。\nこの辞典は、ダンジョンの謎を解明し、探索者たちの安全を確保するための重要なツールである。',
 
-	authorName: '',
-	authorDescription: ''
+	authorName: '猫乃わん太',
+	authorDescription: 'ぬいぐるみ系VTuber。\nブラックアンドタンの柴犬のようなぽってりとした二足歩行のぬいぐるみの姿をしている。\n語尾に「わん」をつけるのが特徴。\n一人称は「ボク」',
 }
